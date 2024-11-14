1f219e349a574ea7195f191babfc359cb7a505d0
ankahara <hecatombe100@gmail.com>
2024-11-14 13:36:21 -0600

    mejoras en el router

--- src/layout/Main.tsx
+++ src/layout/Main.tsx
@@ -1,13 +1,14 @@
 import { ToastContainer } from 'react-toastify';
 import Loading from '../infrastructure/components/Loading';
+import { Outlet } from 'react-router-dom';
 
-const Main = ({children}: {children: React.ReactNode}) => {
+const Main = ({children}: {children?: React.ReactNode}) => {
   return (
     <main className="w-full min-h-screen bg-gradient-to-bl from-blue-950 to-blue-400">
       <Loading>
         <div className="w-full h-full grid place-items-center">
           <div className='my-12'>
-            {children}
+            <Outlet />
           </div>
           <ToastContainer autoClose={1500} />
         </div>
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import Main from "../layout/Main";
 import HomePage from "../pages/HomePage";
 import LoginPage from "../pages/LoginPage";
@@ -6,6 +6,10 @@ import PrivateLayout from "../layout/private/Private";
 import CreatePage from "../pages/create/CreatePage";
 
 export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigate to="/home" />,
+  },
   {
     path: "/home",
     element: <PrivateLayout><HomePage /></PrivateLayout>,
