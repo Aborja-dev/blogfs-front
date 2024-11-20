d6fbf771ac3185636b1c0b6d21dae93ea47aea89
ankahara <hecatombe100@gmail.com>
2024-11-20 00:57:50 -0600

    refactorizacion de env variables

--- src/service/api-gateway.ts
+++ src/service/api-gateway.ts
@@ -4,7 +4,9 @@ import { blogMapper } from "../domain/mapper/blog";
 import { PrivateResource } from "../domain/clases/api-resource";
 import { IUser } from "../domain/schema/users/types";
 
-const baseUrl =  'http://localhost:3003/api'
+const baseUrl =  import.meta.env.NODE_ENV === 'development' ? 'http://localhost:3003/api' : import.meta.env.VITE_BASE_URL
+console.log('base url',baseUrl);
+
 
 export const Login = async ({username, password}: {username: string, password: string}): Promise<ISesion> => {
     const {data} = await axios.post(`${baseUrl}/users/login`, {username, password})
@@ -26,6 +28,6 @@ export const GetUserDetails = async (id: string): Promise<ISesion> => {
     }
 }
 
-export const requestBlog = new PrivateResource<IBlog>('http://localhost:3003/api/blogs')
-export const requestUser = new PrivateResource<IUser>('http://localhost:3003/api/users')
+export const requestBlog = new PrivateResource<IBlog>(`${baseUrl}/blogs`)
+export const requestUser = new PrivateResource<IUser>(`${baseUrl}/users`)
 
